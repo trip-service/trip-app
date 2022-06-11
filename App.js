c7e77ab3303a15814b2b7603232d076a9bb8cb7d
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-06-12 00:11:32 +0800

    feat: add commit script and add @react-navigation/bottom-tabs package
    add commit script and add @react-navigation/bottom-tabs package
    
    #1

--- App.js
+++ App.js
@@ -4,20 +4,20 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NativeBaseProvider} from 'native-base';
 import HomeScreen from '~/screens/HomeScreen';
 import LoginScreen from '~/screens/LoginScreen';
-import SignupScreen from './src/screens/SignupScreen';
+import SignupScreen from '~/screens/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
+    <NavigationContainer>
+        <NativeBaseProvider>
         <Stack.Navigator>
+          <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
-      </NavigationContainer>
     </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
