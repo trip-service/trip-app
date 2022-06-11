c5fc9254a7ed98352ecfdd5ed99b2c252a611abf
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-06-11 17:10:46 +0800

    feat: add login signup form screen
    add login signup form screen and native-basev3
    
    #1

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailScreen from './src/screens/DetailScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NativeBaseProvider} from 'native-base';
+import HomeScreen from '~/screens/HomeScreen';
+import LoginScreen from '~/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,17 +1,107 @@
+// In index.js of a new project
+import React, {useState} from 'react';
+import {
+  IconButton,
+  HStack,
+  VStack,
+  Text,
+  Icon,
+  Box,
+  Input,
+  FlatList,
+  Checkbox,
+} from 'native-base';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Entypo from '@expo/vector-icons/Entypo';
 
-import React from 'react';
-import { Button, View, Text } from 'react-native';
-
-function HomeScreen({ navigation }) {
+const renderItem = ({item, index, handleUpdateTask, handleDeleteTask}) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <Box
+      borderBottomWidth="1"
+      _dark={{
+        borderColor: 'gray.600',
+      }}
+      borderColor="coolGray.200"
+      pl="4"
+      pr="5"
+      py="2">
+      <HStack w="100%" justifyContent="space-between" alignItems="center">
+        <Checkbox
+          isChecked={item.isCompleted}
+          onChange={() => handleUpdateTask({index, ...item, isCompleted: !item.isCompleted})}
+          value={item.title}>
+          <Text
+            mx="2"
+            strikeThrough={item.isCompleted}
+            _light={{
+              color: item.isCompleted ? 'gray.400' : 'coolGray.800',
+            }}
+            _dark={{
+              color: item.isCompleted ? 'gray.400' : 'coolGray.50',
+            }}>
+            {item.title}
+          </Text>
+        </Checkbox>
+        <IconButton
+          size="sm"
+          colorScheme="trueGray"
+          icon={
+            <Icon as={Entypo} name="minus" size="xs" color="trueGray.400" />
+          }
+          onPress={() => handleDeleteTask({ index })}
+        />
+      </HStack>
+    </Box>
   );
-}
+};
+
+const HomeScreen = (props) => {
+  const [taskTitle, setTaskTitle] = useState('');
+
+  return (
+    <Box
+    _dark={{
+      borderColor: 'gray.600',
+    }}
+    borderColor="coolGray.200"
+    >
+      <VStack space={4}>
+        <HStack space={2}>
+          <Input
+            flex={1}
+            onChangeText={(v) => setTaskTitle(v)}
+            value={taskTitle}
+            placeholder="Add Task"
+          />
+          <IconButton
+            borderRadius="sm"
+            variant="solid"
+            icon={
+              <Icon
+                as={FontAwesome}
+                name="plus"
+                size="sm"
+                color="warmGray.50"
+              />
+            }
+            onPress={() => {
+              if (taskTitle !== '') {
+                const payload = {title: taskTitle};
+                props.handleAddTask(payload);
+                setTaskTitle('');
+              }
+            }}
+          />
+        </HStack>
+      </VStack>
+      <FlatList
+        style={{marginBottom: 64}}
+        data={props.tasks}
+        keyExtractor={(item, index) => `${item.title}-${index}`}
+        renderItem={(args) => renderItem({...args, handleUpdateTask: props.handleUpdateTask, handleDeleteTask: props.handleDeleteTask })}
+      />
+    </Box>
+  );
+};
 
 export default HomeScreen;
