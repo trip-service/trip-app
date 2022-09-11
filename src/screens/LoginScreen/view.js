cbd9b8eb5968541c583c46baf297b9772a88dbe1
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-09-11 20:49:49 +0800

    feat: 串接 google 登入完成
    #17

--- src/screens/LoginScreen/view.js
+++ src/screens/LoginScreen/view.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import { StyleSheet, View, Text, Dimensions } from 'react-native';
-import { useTheme } from 'react-native-paper';
 import { Avatar, Button } from 'react-native-paper';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import axios from 'axios';
+
+WebBrowser.maybeCompleteAuthSession();
 
 const { width } = Dimensions.get('window');
 
 const HomeScreen = () => {
-  const theme = useTheme();
-  console.log(theme);
+  const [accessToken, setAccessToken] = React.useState();
+  const [userInfo, setUserInfo] = React.useState();
+  const [message, setMessage] = React.useState();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "840924562253-o8r0c4mq2qe0mpjkbljinvhjf27qm21v.apps.googleusercontent.com",
+    iosClientId: "840924562253-06jh0clfulq694p0t9vtfnp0ovomcaoj.apps.googleusercontent.com",
+    expoClientId: "840924562253-4afj41f42scidkcq25ng00ca232c95u4.apps.googleusercontent.com",
+  });
+
+  React.useEffect(() => {
+    setMessage(JSON.stringify(response));
+    if (response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
+    }
+  }, [response]);
+
+
+  async function getUserData() {
+    try {
+      console.log("🚀 ~ file: view.js ~ line 31 ~ getUserData ~ getUserData")
+      let userInfoResponse = await axios.get("https://www.googleapis.com/userinfo/v2/me", {
+        headers: { Authorization: `Bearer ${accessToken}`}
+      });
+      console.log("🚀 ~ file: view.js ~ line 35 ~ getUserData ~ userInfoResponse.data", userInfoResponse.data)
+      setUserInfo(userInfoResponse.data);
+    } catch(error) {
+      console.log("🚀 ~ file: view.js ~ line 35 ~ getUserData ~ error", error)
+      
+    }
+    
+  }
+
+  function showUserInfo() {
+    if (userInfo) {
+      return (
+        <View style={styles.userInfo}>
+          <Image source={{uri: userInfo.picture}} style={styles.profilePic} />
+          <Text>Welcome {userInfo.name}</Text>
+          <Text>{userInfo.email}</Text>
+        </View>
+      );
+    }
+  }
+
   return (
     <View style={styles.containerStyle}>
       <View>
@@ -25,7 +72,9 @@ const HomeScreen = () => {
         <Text>走出屬於你的旅程</Text>
       </View>
       <View style={styles.marginTop26}>
-        <Button mode="contained" onPress={() => console.log('Pressed')}>
+        <Button
+          mode="contained"
+          onPress={accessToken ? getUserData : () => { promptAsync({useProxy: true}) }}>
           以 Google 進入
         </Button>
       </View>
